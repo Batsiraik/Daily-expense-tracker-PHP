c76b3967894d1b0d99f42945ecc4a81078ba0b34
batsie <46093695+Batsiraik@users.noreply.github.com>
2020-07-09 02:56:02 +0300

    Update dbconnection.php

--- includes/dbconnection.php
+++ includes/dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "detsdb");
+$con=mysqli_connect("localhost", "root", "", "yourdbname");
 if(mysqli_connect_errno()){
 echo "Connection Fail".mysqli_connect_error();
 }
