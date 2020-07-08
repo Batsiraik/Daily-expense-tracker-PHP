2d4b9af4c3282de0f5bdc3d2f4fbe45d14960332
batsie <46093695+Batsiraik@users.noreply.github.com>
2020-07-09 02:53:39 +0300

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,3 @@
 <div class="col-sm-12">
-				<p class="back-link">Daily Expense Tracker <a href="https://phpgurukul.com">phpgurukul</a></p>
+				<p class="back-link">Daily Expense Tracker <a href="https://batsiebrilliant.com">phpgurukul</a></p>
 			</div>
